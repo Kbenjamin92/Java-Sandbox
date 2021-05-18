71bd6c3b12782a6c34a59b7d7009be83558f39b7
kbenjamin92 <kipp.benjamin@bryanuniversity.edu>
2021-05-18 01:36:59 -0400

    working on the cart functionality

--- Main.java
+++ Main.java
@@ -12,14 +12,20 @@ allow users to enter new items to their cart and calculate the total at check ou
 */
 
 public class Main {
+    String[] storeItems = {"MacBook", "Surface", "Linux", "Dell", "Samsung"};
     String[] items = {};
     int[] price = {};
     String[] cart = {};
 
-    static void addToCart() {
-        System.out.println("Welcome to my terminal store, ");
+     public void addToCart() {
+        System.out.println("Welcome to my terminal store. \nHello shopper, chose from our selected items to add to your cart. \nYou can add items to your cart and get the total. Happy Shopping!!!");
+        for (int i = 0; i < storeItems.length; i++) {
+            System.out.println(storeItems[i]);
+        }
         Scanner userInput = new Scanner(System.in);
-
+        System.out.println("What items would you like to add? ");
+        String value = userInput.next().trim();
+        System.out.println(value + " has been saved in your cart!");
     }
 
     static void addPrice() {
@@ -27,6 +33,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
+         Main cartObj = new Main();
+        cartObj.addToCart();
     }
 }
