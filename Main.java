7bd240b212e8d6cc5ad835c8354773fe51c0fa07
kbenjamin92 <kipp.benjamin@bryanuniversity.edu>
2021-05-18 01:07:57 -0400

    first java commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+package com.kippbenjamin;
+import java.util.Scanner;
+/*
+Create a shopping cart program with Java in the terminal
+allow users to enter new items to their cart and calculate the total at check out.
+
+- Get data from user
+- Need to dynamically add items to an array
+- Need to be able to add the price of items to another array
+- Add error handling for incorrect users inputs
+- Add message for user
+*/
+
+public class Main {
+    String[] items = {};
+    int[] price = {};
+    String[] cart = {};
+
+    static void addToCart() {
+        System.out.println("Welcome to my terminal store, ");
+        Scanner userInput = new Scanner(System.in);
+
+    }
+
+    static void addPrice() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Practice.java
@@ -0,0 +1,7 @@
+package com.kippbenjamin;
+
+public class Practice {
+    public static void practice(String [] args) {
+        System.out.println("This is a new file");
+    }
+}
